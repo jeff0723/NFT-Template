c562b2294d02ff4a881de5f34959526cc0db95e3
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-02-04 20:19:49 +0800

    add package.json script

--- packages/hardhat/ipfs/uploadUnrevealed.sh
+++ packages/hardhat/ipfs/uploadUnrevealed.sh
@@ -1,6 +1,7 @@
-upload_file_name=$1
-upload_file_amount=$2
-upload_metadata_name=$3
+source ../.env
+upload_file_name=$UPLOAD_UNREVEALED_FILE_NAME
+upload_file_amount=$MAX_SUPPLY
+upload_metadata_name=$NAME
 echo "generate unrevealed metadata with image CID: $upload_file_name"
 unrevealed_cid=$(node ipfsUploadSingleFile.js $upload_file_name)
 node generateUnrevealedMetadata.js $upload_file_amount $upload_metadata_name $upload_file_name
