d3ec1e5d47f0ca54e19218247c2e437ea3792829
jeffreylin0723 <qwertzxc1478963@gmail.com>
2022-02-17 11:44:58 +0800

    change bash script

--- packages/hardhat/ipfs/uploadRevealed.sh
+++ packages/hardhat/ipfs/uploadRevealed.sh
@@ -1,5 +1,4 @@
 node generateImageHash.js
 node fill.js
 node ipfsUploadFolder.js newMetadata >> revealed_cid.txt
-rm imageHash.json
-rm -rf newMetadata
+
--- packages/hardhat/ipfs/uploadUnrevealed.sh
+++ packages/hardhat/ipfs/uploadUnrevealed.sh
@@ -6,4 +6,3 @@ echo "generate unrevealed metadata with image CID: $upload_file_name"
 unrevealed_cid=$(node ipfsUploadSingleFile.js $upload_file_name)
 node generateUnrevealedMetadata.js $upload_file_amount $upload_metadata_name $unrevealed_cid
 node ipfsUploadFolder.js unrevealed >> unrevealed_cid.txt
-rm -rf unrevealed
